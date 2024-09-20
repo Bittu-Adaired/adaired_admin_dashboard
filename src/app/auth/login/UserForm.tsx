4a6b8d70afa5647979e559fcf449d06f03d2e8a1
Adaired <adaired@kali>
2024-09-20 19:03:52 +0530

    [Policies Updated] - Policies has been updated

--- src/app/auth/login/UserForm.tsx
+++ src/app/auth/login/UserForm.tsx
@@ -72,7 +72,6 @@ export const UserForm = () => {
   });
 
   const formSubmitHandle = async (data: LoginSubmitProp) => {
-    window.location.reload();
     try {
       const login = await axios.post(
         `${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`,
